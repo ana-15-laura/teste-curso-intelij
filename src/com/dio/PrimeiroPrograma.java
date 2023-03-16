404616eac5fa952c6fffb8d554ea334429a02c51
Ana Laura Martins <ana.laura.martins.souto@gmail.com>
2023-03-16 15:52:11 -0300

    criando class livro

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,45 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livros livro = new Livros("O labirinto do fauno", 320);
+        System.out.println(livro);
+
         /*int a = 2;
         int b = 3;
 
         System.out.println("Hello world " + (a+b));*/
+    }
+}
+class Livros {
+    private String nome;
+    private Integer npag;
+
+    public Livros(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
     }
 }
